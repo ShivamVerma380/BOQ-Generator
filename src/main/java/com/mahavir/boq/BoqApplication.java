dc8ac1efbda09adbd2f8f63ce800af935f61b8f1
ShivamVerma380 <shivam.r.verma@seagate.com>
2023-05-22 09:19:03 +0530

    Initial commit
    Signed-off-by: ShivamVerma380 <shivam.r.verma@seagate.com>

---
+++ src/main/java/com/mahavir/boq/BoqApplication.java
@@ -0,0 +1,13 @@
+package com.mahavir.boq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahavir/boq/BoqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahavir.boq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
